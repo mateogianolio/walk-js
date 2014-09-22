3ca8e46d3ef2e7a1f9f2975db9bb7dd94114a718
Mateo Gianolio <mateogianolio@Mateos-MacBook-Air.local>
2014-09-23 01:22:04 +0200

    code cleanup, added hotkeys

--- walk.js
+++ walk.js
@@ -1,5 +1,5 @@
 var canvas, context,
-    state, flip, interval, rate,
+    rate, state, interval, flip,
     count = 0,
     traversal = false,
     running = true;
@@ -8,11 +8,10 @@ function ready() {
   canvas = document.getElementById('walk');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-          
+
   context = canvas.getContext('2d');
           
   rate = 10;
-          
   state = {
     'position': {
       'x': Math.round(canvas.width / 2),
@@ -36,66 +35,60 @@ function ready() {
     ],
     'past': null
   };
-          
+
   interval = setInterval(function() { update(); }, rate);
 }
 
 function update() {
-  if(!running)
+  if (!running)
     return;
           
-  if(traversal) {
+  if (traversal) {
     paint();
     return;
   }
-          
-  try {
-    state.past = JSON.parse(JSON.stringify(state));
-    document.getElementById('callstack').innerHTML = 'call stack: <span red>' + count++ + '</span>';
-  } catch (e) {
-    // maximum call stack exceeded
-    var inputs = document.getElementsByTagName('INPUT');
-    for(i = 0; i < inputs.length; i++)
-      inputs[i].disabled = true;
-            
-    document.getElementById('traversal').innerHTML = 'traversing...';
-            
-    traversal = true;
+  
+  // maximum call stack size exceeded, start traversing
+  if (count++ > 1000) {
+    toggle();
     return;
   }
+  
+  state.past = JSON.parse(JSON.stringify(state));
+  document.getElementById('callstack').innerHTML = 'call stack: <span red>' + count + '</span>';
           
   flip = Math.round(Math.random() - state.direction.bias.x);
   state.direction.x = Math.pow(-1, flip);
           
   flip = Math.round(Math.random() - state.direction.bias.y);
   state.direction.y = Math.pow(-1, flip);
-          
+
   state.position.x += state.step * state.direction.x;
   state.position.y += state.step * state.direction.y;
-          
+
   if (state.position.x < 0) {
     state.position.x = 0;
     state.direction.x *= -1;
   }
-          
+
   if (state.position.x > canvas.width) {
     state.position.x = canvas.width - state.step;
   }
-          
+
   if (state.position.y < 0) {
     state.position.y = 0;
   }
-          
+
   if (state.position.y > canvas.height) {
     state.position.y = canvas.height - state.step;
   }
-          
-  if (state.past != null)
-    paint();
+
+  paint();
 }
 
 function paint() {
   if (!traversal) {
+    // walking about..
     context.lineWidth = state.size;
     context.strokeStyle = 'rgba(' + state.color.join(', ') + ')';
           
@@ -104,19 +97,8 @@ function paint() {
     context.lineTo(state.position.x, state.position.y);
     context.stroke();
     context.closePath();
-  } else if (state.past == null) {
-    var inputs = document.getElementsByTagName('input');
-    for(i = 0; i < inputs.length; i++) {
-      inputs[i].disabled = false;
-      inputs[i].value = 0;
-    }
-              
-    document.getElementById('traversal').innerHTML = '';
-                
-    traversal = false;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    return;
-  } else {
+  } else if (state.past != null) {
+    // traversing..
     context.lineWidth = state.past.size;
     context.strokeStyle = 'rgba(' + state.color[0] + ', ' + state.color[1] + ', ' + state.color[2] + ', 1)';
     
@@ -129,7 +111,36 @@ function paint() {
     state = state.past;
             
     document.getElementById('callstack').innerHTML = 'call stack: <span red>' + count-- + '</span>';
+  } else {
+    toggle();
+    clear();
   }
+}
+
+function toggle() {
+  traversal = !traversal;
+  
+  var inputs = document.getElementsByTagName('INPUT');
+  var input;
+  for(i = 0; i < inputs.length; i++) {
+    input = inputs[i];
+    input.value = 0;
+    input.disabled = !input.disabled;
+  }
+  
+  document.getElementById('traversal').innerHTML = traversal ? 'traversing...' : '';
+}
+
+function clear() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function reset() {
+  traversal = false;
+  count = 0;
+  
+  while(state.past != null)
+    state = state.past;
 }
   
 window.onkeydown = function(event) {
@@ -137,8 +148,17 @@ window.onkeydown = function(event) {
           
   switch (event.keyCode) {
     case 32:
+      // space
       running = !running;
-      document.getElementById('space').innerHTML = '(press <b>space</b> to ' + (running ? '' : 'un') + 'pause)';
+      break;
+    case 82:
+      // 'c'
+      reset();
+      clear();
+      break;
+    case 84:
+      // 't'
+      toggle();
       break;
   }
 };
